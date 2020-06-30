222224db2d0af29dfeea3db7df392bbfb8da6403
Markus Noga <ml@noga.de>
2020-06-30 03:54:31 +0200

    towards #2: outlier replacement for background extraction

--- internal/background.go
+++ internal/background.go
@@ -24,7 +24,10 @@ import (
 type Background struct {
 	Width int32
 	Height int32
-	GridSpacing int32	
+	GridSpacing int32
+	NumCellCols int32
+	NumCellRows int32
+	NumCells    int32	
  	Cells []BGCell
 }
 
@@ -44,29 +47,291 @@ func NewBackground(src []float32, width int32, gridSpacing int32, sigma float32)
 	numCells   :=numCellCols*numCellRows
 	cells      :=make([]BGCell, numCells)
 
-	buffer:=make([]float32, gridSpacing*gridSpacing) // reuse for all grid cells to ease GC pressure
+	b=&Background{width, height, gridSpacing, numCellCols, numCellRows, numCells, cells}
+
+	b.init(src, sigma)
+	b.smoothe()
+
+	return b
+}
+
+func (bg *Background) init(src []float32, sigma float32) {
+	buffer:=make([]float32, bg.GridSpacing*bg.GridSpacing) // reuse for all grid cells to ease GC pressure
 
 	// For all grid cells
 	c:=0
-	for yStart:=int32(0); yStart<height; yStart+=gridSpacing {
-		yEnd:=yStart+gridSpacing
-		if yEnd>height { yEnd=height }
+	for yStart:=int32(0); yStart<bg.Height; yStart+=bg.GridSpacing {
+		yEnd:=yStart+bg.GridSpacing
+		if yEnd>bg.Height { yEnd=bg.Height }
 
-		for xStart:=int32(0); xStart<width; xStart+=gridSpacing {
-			xEnd:=xStart+gridSpacing
-			if xEnd>width { xEnd=width }
+		for xStart:=int32(0); xStart<bg.Width; xStart+=bg.GridSpacing {
+			xEnd:=xStart+bg.GridSpacing
+			if xEnd>bg.Width { xEnd=bg.Width }
 
 			// Fit linear gradient to masked source image within that cell
-			cells[c].Fit(src, width, sigma, xStart, xEnd, yStart, yEnd, buffer)
+			bg.Cells[c].Fit(src, bg.Width, sigma, xStart, xEnd, yStart, yEnd, buffer)
 			c++
 		}	
 	}	
 
 	buffer=nil
-
-	return &Background{width, height, gridSpacing, cells}
 }
 
+// Smoothes the background gradient function, by parameter
+func (bg *Background) smoothe() {
+	buffer:=make([]float32, bg.NumCells)
+	ignores:=make([]bool, bg.NumCells)
+	for i,_:=range(ignores) { ignores[i]=false }
+
+	// First iteratively update ignores
+	totalIgnores:=int32(0)
+	ignoresChanged:=int32(1)
+	for ; ignoresChanged>0; {
+		for i,cell:=range bg.Cells { buffer[i]=cell.Alpha }
+		alphaMAD, alphasChanged:=expandIgnores(buffer, ignores, bg.NumCellCols, bg.NumCellRows)
+
+		for i,cell:=range bg.Cells { buffer[i]=cell.Beta }
+		betaMAD, betasChanged:=expandIgnores(buffer, ignores, bg.NumCellCols, bg.NumCellRows)
+
+		for i,cell:=range bg.Cells { buffer[i]=cell.Gamma }
+		gammaMAD, gammasChanged:=expandIgnores(buffer, ignores, bg.NumCellCols, bg.NumCellRows)
+
+		ignoresChanged=alphasChanged+betasChanged+gammasChanged
+		totalIgnores+=ignoresChanged
+		LogPrintf("alpha MAD %f changed %d, beta %f %d, gamma %f %d\n", alphaMAD, alphasChanged, betaMAD, betasChanged, gammaMAD, gammasChanged)
+	}
+	LogPrintf("Total %d ignores\n", totalIgnores)
+
+	// Then replace cells with interpolations
+	for neighbors:=16; neighbors>=0; neighbors-- {
+		for i,cell:=range bg.Cells { buffer[i]=cell.Alpha }
+		interpolate(buffer, ignores, bg.NumCellCols, bg.NumCellRows, neighbors)
+		for i,buf:=range buffer { bg.Cells[i].Alpha=buf }
+
+		for i,cell:=range bg.Cells { buffer[i]=cell.Beta }
+		interpolate(buffer, ignores, bg.NumCellCols, bg.NumCellRows, neighbors)
+		for i,buf:=range buffer { bg.Cells[i].Beta=buf }
+
+		for i,cell:=range bg.Cells { buffer[i]=cell.Gamma }
+		interpolate(buffer, ignores, bg.NumCellCols, bg.NumCellRows, neighbors)
+		for i,buf:=range buffer { bg.Cells[i].Gamma=buf }
+
+		relaxIgnores(buffer, ignores, bg.NumCellCols, bg.NumCellRows, neighbors)
+	}
+
+	buffer, ignores=nil, nil
+}
+
+
+func expandIgnores(params []float32, ignores []bool, width, height int32) (mad float32, numChanged int32) {
+	temp:=[]float32{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+
+	// calculate median prediction for all cells
+	var predicts=make([]float32, len(params))
+	var diffs=make([]float32, len(params))
+    for y:=int32(0); y<height; y++ {
+    	for x:=int32(0); x<width; x++ {
+    		offset:=y*width+x
+    		predict,_:=MedianInterpolation(params, ignores, width, height, x,y, temp)
+    		predicts[offset]=predict
+    		param:=params[offset]
+    		diffs[offset]=float32(math.Abs(float64(param-predict)))
+    	}
+    }
+
+    // calculate mean absolute difference from median prediction
+	mad=MedianFloat32(diffs) // reorders diffs, can no longer use below!
+
+	// update list of outliers to ignore
+	numChanged=int32(0)
+    for y:=int32(0); y<height; y++ {
+    	for x:=int32(0); x<width; x++ {
+    		offset:=y*width+x
+    		if !ignores[offset] {
+    			if float32(math.Abs(float64(params[offset]-predicts[offset])))>=6*mad {
+					ignores[offset]=true
+					numChanged++
+				}
+			}
+		}
+	}
+	predicts, diffs=nil, nil
+	return mad, numChanged
+}
+
+
+// Smoothes a parameter
+func interpolate(params []float32, ignores []bool, width, height int32, neighbors int) {
+	temp:=[]float32{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+
+    for y:=int32(0); y<height; y++ {
+    	for x:=int32(0); x<width; x++ {
+    		offset:=y*width+x
+    		if ignores[offset] {
+	    		predict, numGathered:=MedianInterpolation(params, ignores, width, height, x,y, temp)
+	    		if numGathered>=neighbors {
+	    			params[offset]=predict
+	    		}
+    		}
+    	}
+    }
+}
+
+// Smoothes a parameter
+func relaxIgnores(params []float32, ignores []bool, width, height int32, neighbors int) {
+	temp:=[]float32{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+
+    for y:=int32(0); y<height; y++ {
+    	for x:=int32(0); x<width; x++ {
+    		offset:=y*width+x
+    		if ignores[offset] {
+	    		_, numGathered:=MedianInterpolation(params, ignores, width, height, x,y, temp)
+	    		if numGathered>=neighbors {
+	    			ignores[offset]=false
+	    		}
+    		}
+    	}
+    }
+}
+
+
+// Linearly interpolate parameter from all directions in local 2-neighborhood and return the median thereof
+func MedianInterpolation(params []float32, ignores []bool, width, height, x,y int32, temp []float32) (median float32, numGathered int) {
+	offset:=y*width+x
+	numGathered=0
+
+	// interpolate from left
+	if x>1 {
+		off1, off2:=offset-1, offset-2
+		if !ignores[off1] && !ignores[off2] {
+			temp[numGathered]=2*params[off1]-params[off2]
+			numGathered++    			
+		}
+	}
+	// interpolate from half upper left
+	if x>1 && y>0 {
+		off1, off2, off3:=offset-1, offset-width-1, offset-width-2
+		if !ignores[off1] && !ignores[off2] && !ignores[off3] {
+			temp[numGathered]=params[off1]+params[off2]-params[off3]
+			numGathered++    			
+		}
+	}
+	// interpolate from upper left
+	if x>1 && y>1 {
+		off1, off2:=offset-width-1, offset-2*width-2
+		if !ignores[off1] && !ignores[off2] {
+			temp[numGathered]=2*params[off1]-params[off2]
+			numGathered++    			
+		}
+	}
+	// interpolate from upper half left
+	if x>0 && y>1 {
+		off1, off2, off3:=offset-width, offset-width-1, offset-2*width-1
+		if !ignores[off1] && !ignores[off2] && !ignores[off3] {
+			temp[numGathered]=params[off1]+params[off2]-params[off3]
+			numGathered++    			
+		}
+	}
+	// interpolate from above
+	if y>1 {
+		off1, off2:=offset-width, offset-2*width
+		if !ignores[off1] && !ignores[off2] {
+			temp[numGathered]=2*params[off1]-params[off2]
+			numGathered++    			
+		}
+	}
+	// interpolate from upper half right
+	if y>1 && x<width-1 {
+		off1, off2, off3:=offset-width, offset-width+1, offset-2*width+1
+		if !ignores[off1] && !ignores[off2] && !ignores[off3] {
+			temp[numGathered]=params[off1]+params[off2]-params[off3]
+			numGathered++    			
+		}
+	}
+	// interpolate from upper right
+	if y>1 && x<width-2 {
+		off1, off2:=offset-width+1, offset-2*width+2
+		if !ignores[off1] && !ignores[off2] {
+			temp[numGathered]=2*params[off1]-params[off2]
+			numGathered++    			
+		}
+	}
+	// interpolate from half upper right
+	if y>0 && x<width-2 {
+		off1, off2, off3:=offset+1, offset-width+1, offset-width+2
+		if !ignores[off1] && !ignores[off2] && !ignores[off3] {
+			temp[numGathered]=params[off1]+params[off2]-params[off3]
+			numGathered++    			
+		}
+	}
+	// interpolate from right
+	if x<width-2 {
+		off1, off2:=offset+1, offset+2
+		if !ignores[off1] && !ignores[off2] {
+			temp[numGathered]=2*params[off1]-params[off2]
+			numGathered++    			
+		}
+	}
+	// interpolate from half lower right
+	if y<height-1 && x<width-2 {
+		off1, off2, off3:=offset+1, offset+width+1, offset+width+2
+		if !ignores[off1] && !ignores[off2] && !ignores[off3] {
+			temp[numGathered]=params[off1]+params[off2]-params[off3]
+			numGathered++    			
+		}
+	}
+	// interpolate from lower right
+	if y<height-2 && x<width-2 {
+		off1, off2:=offset+width+1, offset+2*width+2
+		if !ignores[off1] && !ignores[off2] {
+			temp[numGathered]=2*params[off1]-params[off2]
+			numGathered++    			
+		}
+	}
+	// interpolate from lower half right
+	if y<height-2 && x<width-1 {
+		off1, off2, off3:=offset+width, offset+width+1, offset+2*width+1
+		if !ignores[off1] && !ignores[off2] && !ignores[off3] {
+			temp[numGathered]=params[off1]+params[off2]-params[off3]
+			numGathered++    			
+		}
+	}
+	// interpolate from below
+	if y<height-2 {
+		off1, off2:=offset+width, offset+2*width
+		if !ignores[off1] && !ignores[off2] {
+			temp[numGathered]=2*params[off1]-params[off2]
+			numGathered++    			
+		}
+	}
+	// interpolate from below half left
+	if x>0 && y<height-2 {
+		off1, off2, off3:=offset+width, offset+width-1, offset+2*width-1
+		if !ignores[off1] && !ignores[off2] && !ignores[off3] {
+			temp[numGathered]=params[off1]+params[off2]-params[off3]
+			numGathered++    			
+		}
+	}
+	// interpolate from below left
+	if x>1 && y<height-2 {
+		off1, off2:=offset+width-1, offset+2*width-2
+		if !ignores[off1] && !ignores[off2] {
+			temp[numGathered]=2*params[off1]-params[off2]
+			numGathered++    			
+		}
+	}
+	// interpolate from half below left
+	if x>1 && y<height-1 {
+		off1, off2, off3:=offset-1, offset+width-1, offset+width-2
+		if !ignores[off1] && !ignores[off2] && !ignores[off3] {
+			temp[numGathered]=params[off1]+params[off2]-params[off3]
+			numGathered++    			
+		}
+	}
+
+	if numGathered==0 { return 0, 0 }
+	return MedianFloat32(temp[:numGathered]), numGathered
+}
 
 // Render full background into a data array, returning the array
 func (b Background) Render() (dest []float32) {
