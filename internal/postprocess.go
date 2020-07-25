5c6b96d5883cbef1c89f8933ef06fea1379cb9b0
Markus Noga <ml@noga.de>
2020-07-25 11:26:16 +0200

    towards #17: integrated unsharp masking to postprocessing pipeline

--- internal/postprocess.go
+++ internal/postprocess.go
@@ -41,7 +41,9 @@ const (
 )
 
 // Postprocess all light frames with given settings, limiting concurrency to the number of available CPUs
-func PostProcessLights(alignRef, histoRef *FITSImage, lights []*FITSImage, align int32, alignK int32, alignThreshold float32, normalize HistoNormMode, oobMode OutOfBoundsMode, postProcessedPattern string, imageLevelParallelism int32) (numErrors int) {
+func PostProcessLights(alignRef, histoRef *FITSImage, lights []*FITSImage, align int32, alignK int32, alignThreshold float32, 
+	                   normalize HistoNormMode, oobMode OutOfBoundsMode, usmSigma, usmGain, usmThresh float32, 
+	                   postProcessedPattern string, imageLevelParallelism int32) (numErrors int) {
 	var aligner *Aligner=nil
 	if align!=0 {
 		if alignRef==nil || alignRef.Stars==nil || len(alignRef.Stars)==0 {
@@ -55,7 +57,7 @@ func PostProcessLights(alignRef, histoRef *FITSImage, lights []*FITSImage, align
 		sem <- true 
 		go func(i int, lightP *FITSImage) {
 			defer func() { <-sem }()
-			res, err:=postProcessLight(aligner, histoRef, lightP, alignThreshold, normalize, oobMode)
+			res, err:=postProcessLight(aligner, histoRef, lightP, alignThreshold, normalize, oobMode, usmSigma, usmGain, usmThresh)
 			if err!=nil {
 				LogPrintf("%d: Error: %s\n", lightP.ID, err.Error())
 				numErrors++
@@ -76,9 +78,10 @@ func PostProcessLights(alignRef, histoRef *FITSImage, lights []*FITSImage, align
 	return numErrors
 }
 
-// Postprocess a single light frame with given settings.
-// Post-processing includes normalization, alignment determination and resampling in reference frame
-func postProcessLight(aligner *Aligner, histoRef, light *FITSImage, alignThreshold float32, normalize HistoNormMode, oobMode OutOfBoundsMode) (res *FITSImage, err error) {
+// Postprocess a single light frame with given settings. Processing steps can include:
+// normalization, alignment and resampling in reference frame, and unsharp masking 
+func postProcessLight(aligner *Aligner, histoRef, light *FITSImage, alignThreshold float32, normalize HistoNormMode, 
+					  oobMode OutOfBoundsMode, usmSigma, usmGain, usmThresh float32) (res *FITSImage, err error) {
 	// Match reference frame histogram 
 	switch normalize {
 		case HNMNone: 
@@ -91,39 +94,53 @@ func postProcessLight(aligner *Aligner, histoRef, light *FITSImage, alignThresho
 	    	var err error
 	    	light.Stats, err=CalcExtendedStats(light.Data, light.Naxisn[0])
 	    	if err!=nil { return nil, err }
+			LogPrintf("%d: %s\n", light.ID, light.Stats)
 	}
 
+	// Is alignment to the reference frame required?
 	if aligner==nil || aligner.RefStars==nil || len(aligner.RefStars)==0 {
+		// Generally not required
 		light.Trans=IdentityTransform2D()		
-		return light, nil
-	} else if light.Stars==nil || len(light.Stars)==0 {
-		// skip alignment and warn if the current frame lacks stars
-		LogPrintf("%d: no stars found, skipping alignment", light.ID)
-		light.Trans=IdentityTransform2D()		
-		return light, nil
 	} else if (len(aligner.RefStars)==len(light.Stars) && (&aligner.RefStars[0]==&light.Stars[0])) {
-		// skip alignment if this is the reference frame
+		// Not required for reference frame itself
 		light.Trans=IdentityTransform2D()		
-		return light, nil
+	} else if light.Stars==nil || len(light.Stars)==0 {
+		// No stars - skip alignment and warn
+		LogPrintf("%d: warning: no stars found, skipping alignment", light.ID)
+		light.Trans=IdentityTransform2D()		
+	} else {
+		// Alignment is required
+		// determine out of bounds fill value
+		var outOfBounds float32
+		switch(oobMode) {
+			case OOBModeNaN:         outOfBounds=float32(math.NaN())
+			case OOBModeRefLocation: outOfBounds=histoRef.Stats.Location
+			case OOBModeOwnLocation: outOfBounds=light   .Stats.Location
+		}
+
+		// Determine alignment of the image to the reference frame
+		trans, residual := aligner.Align(light.Naxisn, light.Stars, light.ID)
+		if residual>alignThreshold {
+			msg:=fmt.Sprintf("%d:Skipping image as residual %g is above limit %g", light.ID, residual, alignThreshold)
+			return nil, errors.New(msg)
+		} 
+		light.Trans, light.Residual=trans, residual
+		LogPrintf("%d: Transform %v; oob %.3g residual %.3g\n", light.ID, light.Trans, outOfBounds, light.Residual)
+
+		// Project image into reference frame
+		light, err= light.Project(aligner.Naxisn, trans, outOfBounds)
+		if err!=nil { return nil, err }
 	}
 
-	// determine out of bounds fill value
-	var outOfBounds float32
-	switch(oobMode) {
-		case OOBModeNaN:         outOfBounds=float32(math.NaN())
-		case OOBModeRefLocation: outOfBounds=histoRef.Stats.Location
-		case OOBModeOwnLocation: outOfBounds=light   .Stats.Location
+	// apply unsharp masking, if requested
+	if usmGain>0 {
+		light.Stats, err=CalcExtendedStats(light.Data, light.Naxisn[0])
+		if err!=nil { return nil, err }
+		absThresh:=light.Stats.Location + light.Stats.Scale*usmThresh
+		LogPrintf("%d: Unsharp masking with sigma %.3g gain %.3g thresh %.3g absThresh %.3g\n", light.ID, usmSigma, usmGain, usmThresh, absThresh)
+		light.Data=UnsharpMask(light.Data, int(light.Naxisn[0]), usmSigma, usmGain, light.Stats.Min, light.Stats.Max, absThresh)
+		light.Stats=CalcBasicStats(light.Data)
 	}
 
-	// Determine alignment of the image to the reference frame
-	trans, residual := aligner.Align(light.Naxisn, light.Stars, light.ID)
-	if residual>alignThreshold {
-		msg:=fmt.Sprintf("%d:Skipping image as residual %g is above limit %g", light.ID, residual, alignThreshold)
-		return nil, errors.New(msg)
-	} 
-	light.Trans, light.Residual=trans, residual
-	LogPrintf("%d: Transform %v; residual %.3g\n", light.ID, light.Trans, light.Residual)
-
-	// Project image into reference frame
-	return light.Project(aligner.Naxisn, trans, outOfBounds)
+	return light, nil
 }
