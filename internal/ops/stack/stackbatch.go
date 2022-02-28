5813a262bd2262f4f6858bc52598db13a60f43c9
Markus Noga <ml@noga.de>
2022-02-28 13:20:45 +0100

    removed dead code

--- internal/ops/stack/stackbatch.go
+++ internal/ops/stack/stackbatch.go
@@ -17,28 +17,10 @@
 package stack
 
 import (
-	// "errors"
-	// "fmt"
-	// "io"
-	// "math"
-	// "runtime/debug"
-	// "github.com/mlnoga/nightlight/internal/fits"
 	"github.com/mlnoga/nightlight/internal/ops"
 	"github.com/mlnoga/nightlight/internal/ops/pre"
 	"github.com/mlnoga/nightlight/internal/ops/ref"
 )
-
-
-// type OpStackSingleBatch struct {
-// 	PreProcess      *pre.OpPreProcess      `json:"preProcess"`
-// 	SelectReference *ref.OpSelectReference `json:"selectReference"`
-// 	PostProcess     *post.OpPostProcess     `json:"postProcess"`
-// 	Stack           *OpStack           `json:"stack"`
-// 	StarDetect      *pre.OpStarDetect      `json:"starDetect"` 
-// 	Save            *ops.OpSave            `json:"save"`
-// 	MaxThreads       int64             `json:"-"`
-// }
-// var _ ops.OperatorJoinFiles = (*OpStackSingleBatch)(nil) // Compile time assertion: type implements the interface
 
 
 func NewOpStackBatch(opPreProc *ops.OpSequence, opSelectReference *ref.OpSelectReference, 
@@ -48,89 +30,3 @@ func NewOpStackBatch(opPreProc *ops.OpSequence, opSelectReference *ref.OpSelectR
 		opPreProc, opSelectReference, opPostProc, opStack, opStarDetect, opSave,
 	)
 }
-
-/*
-// Stack a given batch of files, using the reference provided, or selecting a reference frame if nil.
-// Returns the stack for the batch, and updates reference frame internally
-func (op *OpStackSingleBatch) Apply(opLoads []*ops.OpLoad, logWriter io.Writer) (fOut *fits.Image, err error) {
-	if len(opLoads)==0 {
-		return nil, errors.New("No frames to preprocess, postprocess and stack")
-	}
-
-	// Preprocess light frames (subtract dark, divide flat, remove bad pixels, detect stars and HFR)
-	fmt.Fprintf(logWriter, "\nPreprocessing %d frames...\n", len(opLoads))
-
-	if op.PreProcess==nil { return nil, errors.New("Missing preprocessing parameters") }
-	opParallelPre:=ops.NewOpParallel(op.PreProcess, op.MaxThreads)
-	lights, err:=opParallelPre.ApplyToFiles(opLoads, logWriter)
-	if err!=nil { return nil, err }
-	lights=RemoveNils(lights) // Remove nils from lights, in case of read errors
-	debug.FreeOSMemory()					
-	if len(lights)==0 {
-		return nil, errors.New("No frames left to postprocess and stack")
-	}
-
-	avgNoise:=float32(0)
-	for _,l:=range lights {
-		avgNoise+=l.Stats.Noise()
-	}
-	avgNoise/=float32(len(lights))
-	fmt.Fprintf(logWriter, "Average input frame noise is %.4g\n", avgNoise)
-
-	// Select reference frame, unless one was provided from prior batches
-	if op.SelectReference==nil { return nil, errors.New("Missing reference frame parameters") }
-	if op.SelectReference.Frame==nil {
-		lights, err=op.SelectReference.Apply(lights, logWriter)
-		if err!=nil { return nil, err }
-		op.PostProcess.Align.Reference    =op.SelectReference.Frame
-	    op.PostProcess.Normalize.Reference=op.SelectReference.Frame
-	}
-
-	// Post-process all light frames (align, normalize)
-	fmt.Fprintf(logWriter, "\nPostprocessing %d frames...\n", len(lights))
-
-	if op.PostProcess==nil { return nil, errors.New("Missing postprocessing parameters") }
-	opParallelPost:=ops.NewOpParallel(op.PostProcess, op.MaxThreads)
-	lights, err=opParallelPost.Apply(lights, logWriter)
-	if err!=nil { return nil, err }
-	lights=RemoveNils(lights) // Remove nils from lights, in case of alignment errors
-	debug.FreeOSMemory()					
-	if len(lights)==0 {
-		return nil, errors.New("No frames left to stack")
-	}
-
-	// Tell the stacker the location of the reference frame. Used to fill in NaN pixels when stacking
-	op.Stack.RefFrameLoc=0
-	if op.PostProcess.Align.Reference!=nil && op.PostProcess.Align.Reference.Stats!=nil {
-		op.Stack.RefFrameLoc=op.PostProcess.Align.Reference.Stats.Location()
-	}
-
-	// Perform the stack
-	if op.Stack==nil { return nil, errors.New("Missing stacking parameters") }
-	fmt.Fprintf(logWriter, "\nStacking %d frames...\n", len(lights))
-	fOut, err=op.Stack.Apply(lights, logWriter)
-	if err!=nil { return nil, err }
-
-	// Free memory
-	numFrames:=len(lights)
-	lights=nil
-	debug.FreeOSMemory()
-
-
-	// Find stars in the newly stacked batch and report out on them
-	if op.StarDetect==nil { return nil, errors.New("Missing star detection parameters") }
-	fOut, err=op.StarDetect.Apply(fOut, logWriter)
-	fmt.Fprintf(logWriter, "Batch %d stack: Stars %d HFR %.2f Exposure %gs %v\n", 0, len(fOut.Stars), fOut.HFR, fOut.Exposure, fOut.Stats)
-	// FIXME: Batch ID
-
-	expectedNoise:=avgNoise/float32(math.Sqrt(float64(numFrames)))
-	fmt.Fprintf(logWriter, "Batch %d expected noise %.4g from stacking %d frames with average noise %.4g\n",
-				0, expectedNoise, numFrames, avgNoise )
-	// FIXME: Batch ID
-
-	// Save batch interim results, if desired
-	if fOut, err=op.Save.Apply(fOut, logWriter); err!=nil { return nil, err}
-
-	return fOut, nil
-}
-*/
